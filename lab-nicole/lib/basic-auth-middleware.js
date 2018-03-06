a61c6360b17211d7f5eea727fcee4e9fa5d88207
nicoleweese <nweese@ymail.com>
2018-03-05 22:39:15 -0800

    started tests, failing

---
+++ lab-nicole/lib/basic-auth-middleware.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const createError = require('http-errors');
+const debug = require('debug')('cfgram:basic-auth-middleware');
+
+module.exports = function(req, res, next) {
+  debug('basic auth');
+
+  var authHeader = req.headers.authorization;
+
+  if(!authHeader) next(createError(401, 'authorization header required'));
+
+  var base64str = authHeader.split('Basic ')[1];
+  if(!base64str) next(createError(401, 'username and password required'));
+
+  var utf8str = Buffer.from(base64str, 'base64').toString();
+  var authArray = utf8str.split(':');
+
+  req.auth = {
+    username: authArray[0],
+    password: authArray[1],
+  };
+
+  if (!req.auth.username) next(createError(401, 'username required'));
+  if (!req.auth.password) next(createError(401, 'password required'));
+
+  next();
+};
---
+++ lab-nicole/route/auth-router.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const jsonParser = require('body-parser').json();
+const debug = require('debug')('cfgram:auth-router');
+const Router = require('express').Router;
+// don't need all of express so we just pull in the router itself
+const basicAuth = require('../lib/basic-auth-middleware.js');
+const createError = require('http-errors');
+const User = require('../model/user.js');
+
+const authRouter = module.exports = Router();
+
+authRouter.post('/api/signup', jsonParser, function(req, res, next) {
+  debug('POST: /api/signup');
+
+  console.log('req.body', req.body);
+  
+  if (req.body.username === undefined || req.body.email === undefined) return next(createError(400));
+  
+  let password = req.body.password;
+  delete req.body.password;
+
+  let user = new User(req.body);
+  user.generatePasswordHash(password)
+    .then( user => user.save())
+    .then( user => user.generateToken())
+    .then( token => res.send(token))
+    .catch( err => {
+      next(createError(404, err.message));
+    });
+});
+
+authRouter.get('/api/signin', basicAuth, function(req, res, next) {
+  debug('GET: /api/signin');
+
+  User.findOne({ username: req.auth.username })
+    .then( user => {
+      if (!user) return next(createError(401));
+      console.log('user', user);
+      console.log('req.auth', req.auth);
+      console.log('req.auth.password', req.auth.password);
+      
+      return user.comparePasswordHash(req.auth.password);
+    })
+    .then( user => user.generateToken())
+    .then( token => res.send(token))
+    .catch( err => {
+      if (err.name === 'ValidationError') return next(createError(401));
+      return next(err);
+    });
+});
